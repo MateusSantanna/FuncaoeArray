2940b3b28ee1e280cb60b9570bf3fbc0598b3531
MateusSantanna <mateussantanna2001@outlook.com>
2022-04-19 16:44:42 -0300

    Array

---
+++ Função e Array.js
@@ -0,0 +1,21 @@
+function competidores (firstPlace, secondPlace, lastPlace){
+     if(secondPlace == "Daniel"){
+         let ultrapassagem = firstPlace
+         firstPlace = "Daniel";
+         secondPlace = ultrapassagem;
+     }
+
+     if(lastPlace == "Daniel"){
+        let ultrapassagem = secondPlace
+        secondPlace = "Daniel";
+        lastPlace = ultrapassagem;
+    }
+
+    let primeiro = "o Primeiro lugar foi: " + firstPlace;
+    let segundo = "o Segundo lugar foi: " + secondPlace;
+    let terceiro = "o Terceiro lugar foi: " + lastPlace;
+    let arrFinal = [];
+    arrFinal.push(primeiro, segundo, terceiro)
+   
+    return (primeiro, segundo, terceiro)
+}
